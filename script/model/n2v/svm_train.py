2d2671b470e658c223295e610e14e765fc2ccd54
yokoyang <yokocc255@163.com>
2019-10-15 18:27:43 +0800

    train v4

--- script/model/n2v/svm_train.py
+++ script/model/n2v/svm_train.py
@@ -13,6 +13,8 @@ from sekg.ir.models.n2v.svm.filter_semantic_tfidf_n2v import FilterSemanticTFIDF
 from definitions import OUTPUT_DIR, DATA_DIR
 from sekg.ir.preprocessor.base import Preprocessor
 
+from util.path_util import PathUtil
+
 
 class SVMTrainer():
     def __init__(self, graph_data):
@@ -26,7 +28,7 @@ class SVMTrainer():
         self.processor = Preprocessor()
         self.doc_collection = PreprocessMultiFieldDocumentCollection.create_from_doc_collection(self.processor,
                                                                                                 self.collection)
-        self.pretrain_node2vec_path = None
+        self.pretrain_node2vec_path = PathUtil.node2vec(pro_name="jdk8", version="v4", weight="unweight")
 
     def train(self):
         self.model.train_from_doc_collection_with_preprocessor(self.doc_collection, pretrain_node2vec_path=)
