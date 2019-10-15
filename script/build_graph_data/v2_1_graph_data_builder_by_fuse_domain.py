0862bf1b20d919a3fdc4f33bb821871a43fa5a93
吕小巷 <1547554745@qq.com>
2019-10-15 16:57:36 +0800

    修改了部分结构

--- script/build_graph_data/v2_1_graph_data_builder_by_fuse_domain.py
+++ script/build_graph_data/v2_1_graph_data_builder_by_fuse_domain.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-import sys
-sys.path.append('/home/fdse/lvgang/APIKGSummaryV1')
-
 from graph.builder.graph_builder import CodeGraphBuilder
 from util.path_util import PathUtil
 
