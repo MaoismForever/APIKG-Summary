12aa4fff2ed20ec335e7703fd32e1c18a393c08b
yokoyang <yokocc255@163.com>
2019-10-16 10:08:04 +0800

    train v4 svm

--- script/model/n2v/svm_train.py
+++ script/model/n2v/svm_train.py
@@ -10,6 +10,7 @@ from sekg.graph.exporter.graph_data import GraphData
 from sekg.graph.exporter.weight_graph_data import WeightGraphData
 from sekg.ir.doc.wrapper import PreprocessMultiFieldDocumentCollection, MultiFieldDocumentCollection
 from sekg.ir.models.n2v.svm.filter_semantic_tfidf_n2v import FilterSemanticTFIDFNode2VectorModel
+from sekg.ir.models.tf_idf import TFIDFModel
 
 from definitions import OUTPUT_DIR, DATA_DIR
 from sekg.ir.preprocessor.base import Preprocessor
@@ -19,11 +20,13 @@ from util.path_util import PathUtil
 
 class SVMTrainer():
     def __init__(self):
+        pro_name="jdk8"
+        version="v4"
         self.graph_data = GraphData.load(
-            str(Path(DATA_DIR) / "graph" / ("{pro}.{version}.graph".format(pro="jdk8", version="v4"))))
+            str(Path(DATA_DIR) / "graph" / ("{pro}.{version}.graph".format(pro=pro_name, version="v4"))))
         model_dir_path = Path(OUTPUT_DIR) / 'svm_model'
         self.model = FilterSemanticTFIDFNode2VectorModel(name="svm", model_dir_path=model_dir_path)
-        self.document_collection_path = Path(DATA_DIR) / 'doc' / 'jdk8' / 'jdk8.v4.dc'
+        self.document_collection_path = Path(DATA_DIR) / 'doc' / pro_name / 'jdk8.v4.dc'
         self.collection = MultiFieldDocumentCollection.load(
             str(self.document_collection_path))
         self.processor = Preprocessor()
@@ -31,12 +34,16 @@ class SVMTrainer():
                                                                                                 self.collection)
         self.pretrain_node2vec_path = PathUtil.node2vec(pro_name="jdk8", version="v4", weight="unweight")
         self.kg_name_searcher_path = str(Path(
-            DATA_DIR) / "graph" / ("{pro}.{version}.namesearcher".format(pro="jdk8", version="v4")))
+            DATA_DIR) / "graph" / ("{pro}.{version}.namesearcher".format(pro=pro_name, version="v4")))
+        self.doc_sim_model_path = PathUtil.best_text_sim_model(pro_name=pro_name, version=version)
 
     def train(self):
         self.model.train_from_doc_collection_with_preprocessor(self.doc_collection,
                                                                pretrain_node2vec_path=self.pretrain_node2vec_path,
-                                                               kg_name_searcher_path=self.kg_name_searcher_path)
+                                                               kg_name_searcher_path=self.kg_name_searcher_path,
+                                                               doc_sim_model_path=self.doc_sim_model_path,
+                                                               doc_sim_model_class=TFIDFModel
+                                                               )
         self.model.save(str(OUTPUT_DIR / "sim_models" / "jdk8" / "v4" / "svm"))
 
 
