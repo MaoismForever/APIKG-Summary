5ca8c7c5befeeaf940435cf05bbf30af3d7ad8ba
yokoyang <yokocc255@163.com>
2019-10-16 09:20:11 +0800

    train v4 svm

--- script/model/n2v/svm_train.py
+++ script/model/n2v/svm_train.py
@@ -5,6 +5,7 @@ import re
 from pathlib import Path
 
 import networkx as nx
+from sekg.graph.exporter.graph_data import GraphData
 
 from sekg.graph.exporter.weight_graph_data import WeightGraphData
 from sekg.ir.doc.wrapper import PreprocessMultiFieldDocumentCollection, MultiFieldDocumentCollection
@@ -17,9 +18,9 @@ from util.path_util import PathUtil
 
 
 class SVMTrainer():
-    def __init__(self, graph_data):
-        self.graph_data = graph_data
-        self.docs = []
+    def __init__(self):
+        self.graph_data = GraphData.load(
+            str(DATA_DIR / "graph" / ("{pro}.{version}.graph".format(pro="jdk8", version="v4"))))
         model_dir_path = Path(OUTPUT_DIR) / 'svm_model'
         self.model = FilterSemanticTFIDFNode2VectorModel(name="svm", model_dir_path=model_dir_path)
         self.document_collection_path = Path(DATA_DIR) / 'doc' / 'jdk8' / 'jdk8.v4.dc'
@@ -29,6 +30,11 @@ class SVMTrainer():
         self.doc_collection = PreprocessMultiFieldDocumentCollection.create_from_doc_collection(self.processor,
                                                                                                 self.collection)
         self.pretrain_node2vec_path = PathUtil.node2vec(pro_name="jdk8", version="v4", weight="unweight")
+        self.kg_name_searcher_path = str(
+            DATA_DIR / "graph" / ("{pro}.{version}.namesearcher".format(pro="jdk8", version="v4")))
 
     def train(self):
-        self.model.train_from_doc_collection_with_preprocessor(self.doc_collection, pretrain_node2vec_path=)
+        self.model.train_from_doc_collection_with_preprocessor(self.doc_collection,
+                                                               pretrain_node2vec_path=self.pretrain_node2vec_path,
+                                                               kg_name_searcher_path=self.kg_name_searcher_path)
+        self.model.save(str(OUTPUT_DIR / "sim_models" / "jdk8" / "v4" / "svm"))
