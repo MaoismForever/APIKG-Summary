b13394585d5dbeab0290de44193d650e5c1dcf9d
lanixngdouble <2530468270@qq.com>
2019-10-18 18:54:44 +0800

    修改avg_n2v的test

--- script/model/avg_n2v/test.py
+++ script/model/avg_n2v/test.py
@@ -6,8 +6,8 @@ from util.path_util import PathUtil
 if __name__ == '__main__':
     pro_name = "jdk8"
     version = "v4"
-    # model_dir_path = PathUtil.sim_model(pro_name=pro_name, version=version, model_type="avg_n2v")
-    # model = AVGNode2VectorModel.load(model_dir_path)
+    model_dir_path = PathUtil.sim_model(pro_name=pro_name, version=version, model_type="avg_n2v")
+    model = AVGNode2VectorModel.load(model_dir_path)
     graph_data_path = PathUtil.graph_data(pro_name=pro_name, version=version)
     graph_data: GraphData = GraphData.load(graph_data_path)
     valid_class_ids = graph_data.get_node_ids_by_label("class")
