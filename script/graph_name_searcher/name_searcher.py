601c5999441286b428f1053b298dade96022bc05
lanixngdouble <2530468270@qq.com>
2019-10-18 14:05:24 +0800

    修改name searcher

--- script/graph_name_searcher/name_searcher.py
+++ script/graph_name_searcher/name_searcher.py
@@ -18,4 +18,5 @@ def train_name_searcher(pro_name, version):
 
 
 if __name__ == "__main__":
+    train_name_searcher(pro_name="jdk8", version="v3")
     train_name_searcher(pro_name="jdk8", version="v4")
