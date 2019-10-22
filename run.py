63175548968ae9a79e1a0bd4e85273d755ad1076
lanixngdouble <2530468270@qq.com>
2019-10-22 19:07:46 +0800

    清理了所有代码的正式版本

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from flask import Flask, request, jsonify
 from script.summary.generate_summary import Summary
 from flask_cors import CORS
+from util.path_util import PathUtil
 
 app = Flask(__name__)
 CORS(app)
-summary = Summary()
+pro_name = "jdk8"
+version = "v3_1"
+compound_model_name = "compound_{base_model}+{extra_model}".format(base_model="avg_w2v", extra_model="svm")
+model_dir = PathUtil.sim_model(pro_name=pro_name, version=version, model_type=compound_model_name)
+summary = Summary(pro_name, version, model_dir)
 
 
 @app.route('/createAPISummary/', methods=['POST'])
